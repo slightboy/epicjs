9765666ae507c9206e4665c5d3f4780767e82021
slightboy <github@slightboy.net>
2014-05-29 22:48:17 +0800

    object prototype update

--- test/object.js
+++ test/object.js
@@ -1,8 +1,44 @@
 var System = require('../index.js');
 
-var o = {a:1, b: 2};
+function selectTest()
+{
+  var o = {a:1, b:2};
+  console.log('function: %j', o.select(function(e) { return e; }));
+  console.log('e => e > 1: %j', o.select('e => e'));
+
+  var n = {};
+  console.log('{} function: %j', n.select(function(e) { return e.id; }));
+  console.log('{} e => e > 1: %j', n.select('e => e.id'));
+
+}
 
 
-var r = o.where('e => e > 1');
+function selectmanyTest()
+{
+  var o = {a : [1,2], b:[3,4]};
+    console.log('function: %j', o.selectmany(function(e) { return e; }));
+  console.log('e => e: %j', o.selectmany('e => e'));
+}
 
-console.log(r);
+function whereTest()
+{
+  var o = {a:1, b:2};
+  console.log('function: %j', o.where(function(e) { return e > 1; }));
+  console.log('e => e > 1: %j', o.where('e => e > 1'));
+
+  var n = {};
+  console.log('{} function: %j', n.where(function(e) { return e.id > 1; }));
+  console.log('{} e => e > 1: %j', n.where('e => e.id > 1'));
+
+}
+//selectTest();
+selectmanyTest();
+var a = {a:1, b:2, c:3};
+
+var n = Object.getOwnPropertyNames(a);
+for (var e in n) {
+  console.log(e);
+};
+console.log(n);
+
+//whereTest();
